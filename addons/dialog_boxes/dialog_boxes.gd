1022c317c2bddfd6d6d8fa4e7e432e465181fa59
Jorge Bahamonde <jbahamon@ug.uchile.cl>
2021-03-19 19:08:33 -0300

    Renaming things

--- addons/dialog_boxes/dialog_boxes.gd
+++ addons/dialog_boxes/dialog_boxes.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("SimpleDialogBox", "CanvasLayer", preload("simple_dialog_box.gd"), preload("icon.png"))
+	add_custom_type("DialogBox", "CanvasLayer", preload("dialog_box.gd"), preload("icon.png"))
 
 
 func _exit_tree():
-	remove_custom_type("SimpleDialogBox")
+	remove_custom_type("DialogBox")
